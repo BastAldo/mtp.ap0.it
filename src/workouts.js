69911117515c989e17d48cd6ded58f2e1a818eab
Aldo <aldo-panzano@hotmail.it>
2025-06-20 16:03:24 +0200

    add fake ex

--- src/workouts.js
+++ src/workouts.js
@@ -14,6 +14,15 @@ export const ALL_EXERCISES = [
     tempo: { up: 1, hold: 1, down: 2 } // seconds
   },
   {
+    id: 'pushup FAKE',
+    name: 'Push Up FAKE',
+    type: 'reps',
+    series: 1,
+    reps: 1,
+    rest: 1, // seconds
+    tempo: { up: 1, hold: 1, down: 1 } // seconds
+  },
+  {
     id: 'squat',
     name: 'Squat a corpo libero',
     type: 'reps',
